5d23002057212f7e69e55420b9b9260bd6564eb9
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2025-05-10 17:07:35 +0530

    learn dynamik router

---
+++ third/app/users/page.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+
+const user = () => {
+  return (
+    <div>
+        
+            <Link href="users/1">
+            <h1>user 1</h1>
+            </Link>
+
+            <Link href="users/2">
+            <h1>user 2</h1>
+            </Link>
+
+            <Link href="users/3">
+            <h1>user 3</h1>
+            </Link>
+            <Link href="users/4">
+            <h1>user 4</h1>
+            </Link>
+
+            <Link href="users/5">
+            <h1>user 5</h1>
+
+            </Link>
+       
+    </div>
+  )
+}
+
+export default user
